f05fc8bf405fca80903f5e1aca4979e0cf810f45
Allan Yung <allan.yung@immersivelabs.co.uk>
2019-03-20 16:05:19 +0000

    Remove this

--- travel.go
+++ travel.go
@@ -4,21 +4,6 @@ import (
 	"math/rand"
 )
 
-// Parts ...
-type Parts struct {
-	wheel int
-	oxen  int
-	axel  int
-}
-
-// Inventory ...
-type Inventory struct {
-	money int
-	oxen  int
-	food  int
-	parts Parts
-}
-
 // Implements "Continue on trail" behaviour
 func travel(rand rand.Rand, distance int, date int, inv Inventory) (int, int, Inventory) {
 	return distance + 1, date + 1, inv
